c1ea6b8a9f838700ffe2b15bafbb85d061358cca
Ignacio Molini <ignacio_molini@hotmail.com>
2021-06-28 17:00:12 -0300

    listo

--- app/models/Login.php
+++ app/models/Login.php
@@ -13,24 +13,3 @@ class Login extends Model
     protected $fillable = [
     ];
 }
-
-// <?php
-
-// class Login
-// {
-//     public $id;
-//     public $mail;
-//     public $hora_inicio;
-//     public $hora_fin;
-
-//     public function logearUsuario()
-//     {
-//         $objAccesoDatos = AccesoDatos::obtenerInstancia();
-//         $consulta = $objAccesoDatos->prepararConsulta("INSERT INTO logins (mail, hora_inicio) VALUES (:mail, :hora_inicio)");
-//         $consulta->bindValue(':mail', $this->mail, PDO::PARAM_STR);
-//         $consulta->bindValue(':hora_inicio', $this->hora_inicio, PDO::PARAM_STR);
-//         $consulta->execute();
-
-//         return $objAccesoDatos->obtenerUltimoId();
-//     }
-// }
